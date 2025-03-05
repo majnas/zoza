7b4a64619e2c794cd3a93104d111b96725c76dad
Majid Nasiri <majnasai@gmail.com>
2025-03-05 18:56:13 +1100

    ignore .env

--- zoza/bot.py
+++ zoza/bot.py
@@ -1,4 +1,3 @@
-# bot.py
 import os
 import sys
 import logging
@@ -22,13 +21,13 @@ logger = logging.getLogger(__name__)
 
 # Set up file handler for image URLs
 os.makedirs("./data", exist_ok=True)  # Create data directory if it doesn't exist
-image_url_logger = logging.getLogger('image_url_logger')
-image_url_logger.setLevel(logging.INFO)
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
 fh = logging.FileHandler('./data/image_url.log')
 fh.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(message)s')
 fh.setFormatter(formatter)
-image_url_logger.addHandler(fh)
+logger.addHandler(fh)
 
 # Load environment variables
 TOKEN = os.getenv("BOT_TOKEN")
@@ -95,7 +94,7 @@ async def handle_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
         photo_file = await update.message.photo[-1].get_file()
         image_url = photo_file.file_path
         # Log the image URL
-        image_url_logger.info(image_url)
+        logger.info(image_url)
         async with aiohttp.ClientSession() as session:
             async with session.post(
                 IMAGE_TO_TEXT_URL,
@@ -104,7 +103,7 @@ async def handle_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
             ) as resp:
                 data = await resp.json()
                 extracted_text = data.get("description", )
-                image_url_logger.info("extracted_text: %s", extracted_text)
+                logger.info("extracted_text: %s", extracted_text)
 
     else:
         extracted_text = update.message.text
@@ -143,7 +142,8 @@ async def style_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     elif style == "Technological":
         final_text = f"{extracted_text} Style is Technological"
 
-    await query.edit_message_text(f"Prompt: {final_text} \nGenerate media ...")
+    # Added emoji to indicate process is underway
+    await query.edit_message_text(f"Prompt: {final_text} \n⏳ Generate media ...")
 
     # Call external TEXT_TO_IMAGE API using the Docker service name.
     async with aiohttp.ClientSession() as session:
@@ -154,17 +154,14 @@ async def style_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         ) as resp:
             result = await resp.json()
 
-    # image_urls = ['https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m340/AQNqTTRNPan-aCnMM4pSRXWavUTf7HIi8jwzuZN-WQnNEP1-gMSBqQFZUCKJOd5ZJZuJcu_dwyi4rXrroZVjEiIDaRFuHt9otKwdpGR-BszROCXSTSHaS_rT4iEptXbRy0BgDlU9qX1hHRRcxiB8nWABgy105Q.jpeg?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=110&ccb=9-4&oh=00_AYBn5Dvzuu1I1xt3aG9gmB7SUPJTP8oRyeeEw8xzIZqGBQ&oe=67BE50AD&_nc_sid=5b3566', 'https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m340/AQMBuuJwW9RBZC_hTR-JcdliP77tXBFJdSlXj_bVT0G-LwYlp_9y0mFy7E3EteEWdSdnNaBJKCRtV9wTeKz30lpvECoZl-9jdaYhJF0o8qbS1SZbzT43zAz0a5SIE40IeuTkZg2AcfHp0I4yFBLTPEx2METJ.jpeg?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=104&ccb=9-4&oh=00_AYCboCCG2j26i8Mz2OcDAe-oJwViDcSUnIx_qtjb2uF_VQ&oe=67BE328B&_nc_sid=5b3566', 'https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m340/AQPEXwL7rdoNdDf4buaC8yxqvS7QQViFtrx_lgeH-tCFjysniFUp-QySX_Bopstf5Cv3s14VxWR_B-L2f_jE2CMIULJcHg-9truMAMN0XGcALxyGpZO4hbqNOpIooZKVZvVit9TrRzxuvVybPI7lOciePzlUcg.jpeg?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=101&ccb=9-4&oh=00_AYBZoaRiBh2zvvIKhP5lhcYGF_UiB9xV9sQxbnz2wu3D6g&oe=67BE4144&_nc_sid=5b3566', 'https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m340/AQP9G8arAZhiebTZWONMPXZ2yaIXnGw0P3sdV2HCKtyfFNqbu7MUii9G5pXaB80yF_lIlQ-KDnxu1czl2c5PUXd3Iq-E29Vd8m_4uus1w3g9xKIhpreBh9yon_16CMa3a25lk1jnFIsp5PSrfIUYLYraaK7EHA.jpeg?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=109&ccb=9-4&oh=00_AYC6B2WTWL0t1VdKw5jzgruFKgedFlG-h541tkFr9FdC9A&oe=67BE30F4&_nc_sid=5b3566']
-    # video_urls = ['https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m256/AQNagCN6NcaM_uJfbnBdNnJCf6Ap9vtBd5w5Yy1syK__yBa3adfMdADiv05kXvph9Eq8NSuqVz5d_N_sq6gvHufaBFC3534qoaT1QeFrbnK72EEM1eOyGq7RA26XMyLO.mp4?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=100&ccb=9-4&oh=00_AYAl3ORs5gOppPTLKAiaw7B73RHDCH0d2yhgSr25WJWcZA&oe=67BE1B1A&_nc_sid=5b3566', 'https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m249/AQNcYRK3UbuH6zIgksdPac4Gdoj32zjsKYPcXTy50ciZ7Wok-DSMxpQGkyyGQsa_rnBecEbqyqosbgg96rInhBbK85yuQzwxHsoK9A15iFw1N-7-wGZ0xempfxivpTA9.mp4?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=103&ccb=9-4&oh=00_AYBNmwoxvKoRQp1au9IiCCuziEf3eQo17qTjZQjbE4vZYg&oe=67BE2123&_nc_sid=5b3566', 'https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m251/AQMUlyhuZoM-c91t_BdnLF-nFDXMOxHjGzD4VyRMLtfdz2IRfoKLyXZIbCa_yjliCC_89NUoHcoEH2wuVnc9JhUC9o0pigDZ2lwLoTM3usJ5t7-5rlGop8wrr9Sb-yGL.mp4?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=109&ccb=9-4&oh=00_AYB7KbW4Ss99hiqnX9Cv1Ey5DOsNskx5Fy0RFIbX9nzlDg&oe=67BE2D59&_nc_sid=5b3566', 'https://scontent.fmel3-1.fna.fbcdn.net/o1/v/t0/f2/m338/AQMSN47XiN6CSeNG-lyhLYF1WPJjyly_42K_wF-FXuHJaZ7cM84UbVPEySZsq3ymQcj-jESQ_elLFIr6xUHDYfPs8F16qxcOyuqSZvDP64zLPviHL383rZDpCv6o72gt.mp4?_nc_ht=scontent.fmel3-1.fna.fbcdn.net&_nc_cat=111&ccb=9-4&oh=00_AYBr3toeaPy1ggiQo_QCsVxwSYR4WiP4LO6kida57V-ddg&oe=67BE3293&_nc_sid=5b3566']
-
-    # result = {"image_urls": image_urls, "video_urls": video_urls}
-
     if not result or "image_urls" not in result or "video_urls" not in result:
         await query.edit_message_text("❌ Failed to generate any media. Try again.")
         return ConversationHandler.END  
 
     image_urls = result.get("image_urls")
     video_urls = result.get("video_urls")
+    logger.info(image_urls)
+    logger.info(video_urls)
 
     if not image_urls or not video_urls:
         await query.edit_message_text("❌ There is no image and video generated!")
@@ -173,7 +170,8 @@ async def style_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     video_paths = []
 
     if video_urls:
-        await query.edit_message_text("Downloading videos...")
+        # Added emoji before "Downloading videos..."
+        await query.edit_message_text("⏳ Downloading videos...")
         for idx, video_url in enumerate(video_urls[:4]):  # Limiting to 4 videos
             video_path = await download_video(video_url, f"video_{idx}.mp4")
             if video_path:
@@ -183,7 +181,8 @@ async def style_selection(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
             merged_video_path = "/tmp/merged_video.mp4"
             merged_with_audio_path = "/tmp/merged_with_audio.mp4"
 
-            await query.edit_message_text("Merge videos...")
+            # Added emoji before "Merge videos..."
+            await query.edit_message_text("⏳ Merge videos...")
             if await merge_videos(video_paths, merged_video_path):
                 if await add_audio_to_video(merged_video_path, merged_with_audio_path, path_to_sound):
                     with open(merged_with_audio_path, "rb") as merged_video:
@@ -200,7 +199,8 @@ async def custom_style(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     extracted_text = context.user_data['extracted_text']
     
     final_text = f"{extracted_text} Style is {custom_style}"
-    await update.message.reply_text(f"Prompt: {final_text} \nGenerate media ...")
+    # Added emoji to indicate process is underway
+    await update.message.reply_text(f"Prompt: {final_text} \n⏳ Generate media ...")
 
     # Call external TEXT_TO_IMAGE API using the Docker service name.
     async with aiohttp.ClientSession() as session:
@@ -225,13 +225,15 @@ async def custom_style(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     video_paths = []
 
     if video_urls:
-        await update.message.reply_text("Downloading videos...")
+        # Added emoji before "Downloading videos..."
+        await update.message.reply_text("⏳ Downloading videos...")
         for idx, video_url in enumerate(video_urls[:4]):
             video_path = await download_video(video_url, f"video_{idx}.mp4")
             if video_path:
                 video_paths.append(video_path)
 
         if len(video_paths) > 1:
+            await update.message.reply_text("⏳ Merge videos...")
             merged_video_path = "/tmp/merged_video.mp4"
             merged_with_audio_path = "/tmp/merged_with_audio.mp4"
 
